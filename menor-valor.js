9e37d79e6347fe3bb3bbb7f0dea6ea05ad843a71
Fernanda Vaz <flauta.fernanda@gmail.com>
2024-08-04 14:42:54 -0300

    aula 2 - JS - algorítimos

--- menor-valor.js
+++ menor-valor.js
@@ -1,8 +1,8 @@
 // array com precos dos livros
 const livros = require("./lista-livros");
 // alterar o indice do produto atual - analizado - caso for o mais barato
-function menorValor(arrProdutos, posicaiInicial) {
-  let maisBarato = posicaiInicial;
+function menorValor(arrProdutos, posicaoInicial) {
+  let maisBarato = posicaoInicial;
 
   for (let atual = posicaoInicial; atual < arrProdutos.length; atual++) {
     if (arrProdutos[atual].preco < arrProdutos[maisBarato].preco) {
---
+++ selection-sort-test.js
@@ -0,0 +1,14 @@
+const livros = require('./lista-livros');
+const menorValor = require('./menor-valor');
+
+livros.forEach((_, indice) => {
+    let menor = menorValor(livros, indice)
+   
+    let livroAtual = livros[indice];
+    let livroMenorPreco = livros[menor];
+   
+    livros[indice] = livroMenorPreco
+    livros[menor] = livroAtual 
+   })
+   
+   console.log(livros)
